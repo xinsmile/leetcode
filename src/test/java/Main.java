c6133f6bf28b76f3412c6228b3f21fa317cbecc7
Xingy <614376875@qq.com>
2018-09-20 23:19:25 +0800

    测试冲突合并1

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         String s = "234we2343";
+        String s2 = "34455";//测试冲突合并
         String ss = "ewrewrew";//测试分支合并
 //        System.out.println(s.substring(0,1));
 //        Set<Character> t = new HashSet<Character>();
